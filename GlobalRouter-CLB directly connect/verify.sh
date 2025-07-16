4bc2574f6b1cab8ecb62652b9bef6b21fa5a31c9
kestrelli <lhr1205146370@163.com>
2025-07-16 11:33:29 +0800

    Delete GlobalRouter-CLB directly connect directory

--- GlobalRouter-CLB directly connect/verify.sh
+++
@@ -1,6 +0,0 @@
-# 验证脚本 (verify.sh)
-#!/bin/bash
-CLB_IP=$(kubectl get svc clb-direct-pod -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-echo "验证结果:"
-curl -s http://$CLB_IP | grep remote_addr
-echo "客户端真实IP显示在 remote_addr 字段"
