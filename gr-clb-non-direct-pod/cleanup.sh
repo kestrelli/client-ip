0857521d2bc68c16de62fe41994d05fbbebac0d5
kestrelli <lhr1205146370@163.com>
2025-07-17 12:47:27 +0800

    Update cleanup.sh

--- gr-clb-non-direct-pod/cleanup.sh
+++ gr-clb-non-direct-pod/cleanup.sh
@@ -1,3 +1,8 @@
-#!/bin/bash  
-# 删除整个命名空间（包含所有资源）  
-kubectl delete namespace kestrel-catchip
+#删除 ingress
+kubectl delete ingress real-ip-ingress  -n kestrel-catchip
+#删除 service
+kubectl delete service real-ip-service  -n kestrel-catchip
+# 删除所有 Pod
+kubectl delete pod -n kestrel-catchip --all
+# 删除 Deployment
+kubectl delete deployment real-ip-deployment -n kestrel-catchip
